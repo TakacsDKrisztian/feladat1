9e5628ff53d6d37e4b7c4374b7d7f7d1c6aa6c74
BuildTools <unconfigured@null.spigotmc.org>
2022-09-25 14:03:41 +0200

    Kész 1-40 az első feladatsor

--- feladat.py
+++ feladat.py
@@ -60,10 +60,7 @@ def feladat6():
 
 feladat6()
 
-
-
 import math
-
 
 def feladat7():
     a = int(input('Add meg a derékszögű háromszög A oldalát: '))
@@ -168,7 +165,6 @@ def feladat14():
 
 feladat14()
 
-
 def feladat15():
 
     print('Utazási költségtérítés\n')
@@ -181,7 +177,6 @@ def feladat15():
         print(f'\nKöltségtérítés: {((road_length*consumption*gasoline_price)/100)+25*road_length}')
 
 feladat15()
-
 
 def feladat16():
 
@@ -228,7 +223,6 @@ def feladat19():
 
 feladat19()
 
-
 def feladat20():
 
     number = int(input('Adj meg egy számot: '))
@@ -242,7 +236,6 @@ def feladat20():
 
 feladat20()
 
-
 def feladat21():
 
     number_1 = int(input('Adj meg egy számot: '))
@@ -255,9 +248,9 @@ def feladat21():
     else:
         print(f'{number_1} > {number_2}')
 
-feladat22()
+feladat21()
 
-def feladat21():
+def feladat22():
 
     number = int(input('Adj meg egy számot: '))
 
@@ -267,7 +260,6 @@ def feladat21():
         print('A szám -30 és 40 között nincs benne')
 
 feladat22()
-
 
 def feladat23():
 
@@ -283,7 +275,6 @@ def feladat23():
 
 feladat23()
 
-
 def feladat24():
 
     settlement = input('Adj meg egy települsé nevet: ')
@@ -297,3 +288,196 @@ def feladat24():
     else: print('Hibás adat bevitel!')
 
 feladat24()
+
+def feladat25():
+
+    number = int(input('Adj meg egy számot: '))
+    number2 = int(input('Adj meg egy másik számot: '))
+    operation_signal = input('Add meg a két szám közötti műveleti jelet (+,-,*,/): ')
+
+    if operation_signal == '+': print(f'{number} + {number2} = {number+number2}')
+    elif operation_signal == '-': print(f'{number} - {number2} = {number-number2}')
+    elif operation_signal == '*': print(f'{number} * {number2} = {number*number2}')
+    elif operation_signal == '/': print(f'{number} / {number2} = {number/number2}')
+    else: print('Nem megfelelő műveleti jel!')
+
+feladat25()
+
+def feladat26():
+
+    number = int(input('Add meg a matematikai osztályzatodat: '))
+
+    if number == 1: print('Elégtelen')
+    elif number == 2: print('Elégséges')
+    elif number == 3: print('Közepes')
+    elif number == 4: print('Jó')
+    elif number == 5: print('Jeles')
+    else: print('Hibás adat bevitel!')
+
+feladat26()
+
+def feladat27():
+
+    day_number = int(input('Adj megy nap számot (1-7 között): '))
+
+    if day_number == 1: print('Hétfő')
+    elif day_number == 2: print('Kedd')
+    elif day_number == 3: print('Szerda')
+    elif day_number == 4: print('Csütörtök')
+    elif day_number == 5: print('Péntek')
+    elif day_number == 6: print('Szombat')
+    elif day_number == 7: print('Vasárnap')
+    else: print('Hibás adat bevitel')
+
+feladat27()
+
+def feladat28():
+
+    month_number = int(input('Adj megy hónap számot (1-12 között): '))
+
+    if month_number == 1: print('Január')
+    elif month_number == 2: print('Február')
+    elif month_number == 3: print('Március')
+    elif month_number == 4: print('Április')
+    elif month_number == 5: print('Május')
+    elif month_number == 6: print('Június')
+    elif month_number == 7: print('Július')
+    elif month_number == 8: print('Augusztus')
+    elif month_number == 9: print('Szeptember')
+    elif month_number == 10: print('Október')
+    elif month_number == 11: print('November')
+    elif month_number == 12: print('December')
+    else: print('Hibás adat bevitel')
+
+feladat28()
+
+def feladat29():
+
+    price = int(input('Add meg az áru egység árát: '))
+    amount = int(input('Mennyit szeretnél vásárolni? '))
+    money = int(input('Mennyi pénz van nálad: '))
+
+    if price*amount <= money: 
+        print('Megtudjuk vásárolni a terméket')
+        print(f'A vissza járó összege: {money-(price*amount)}')
+    else:
+        print('Nincs elég pénzed rá:')
+        print(f'Enynit tudnál venni belőle: {(price*amount)//money}')
+
+feladat29()
+
+def feladat30():
+
+    year = int(input('Adj megy egy évszámot: '))
+
+    if year % 4 == 0 and year % 400 == 0: print('Szökőév')
+    else: print('Nem szökő év')
+
+feladat30()
+
+
+
+from itertools import count
+from multiprocessing.connection import answer_challenge
+import random
+
+
+def feladat31():
+
+    number = random.randint(99,1000)
+
+    print(number)
+
+feladat31()
+
+def feladat32():
+
+    number_1 = random.randint(0,25)
+    number_2 = random.uniform(0,25)
+
+    print(f'{number_1}\n{round(number_2,2)}')
+
+feladat32()
+
+def feladat33():
+
+    number_3 = random.randint(15,25)
+    number_4 = random.uniform(15,25)
+
+    print(f'{number_3}\n{round(number_4,2)}')
+
+feladat33()
+
+def feladat34():
+
+    number_5 = random.randint(0,100)
+
+    while number_5%2 >= 0:
+        if number_5%2 == 0:
+            print(number_5)
+            break
+        else:
+            number_5 = random.randint(0,100)
+        
+
+feladat34()    
+
+def feladat35():
+
+    number_6 = random.randint(100,200)
+
+    while number_6%5 >= 0:
+        if number_6%5 == 0:
+            print(number_6)
+            break
+        else:
+            number_6 = random.randint(100,200)
+
+feladat35()
+
+def feladat36():
+    
+    number_7 = random.randint(0,10000000)
+
+    #print(number_7)
+    print(len(str(number_7)))
+
+feladat36()
+
+def feladat37():
+
+    for x in range(6):
+        number_8 = random.randint(0,6)
+        print(f'\n{number_8}')
+
+feladat34()
+
+def feladat38():
+
+    for x in range(5):
+        number_9 = random.randint(0,95)
+        print(f'\n{number_9}')
+
+feladat38()
+
+def feladat39():
+
+    number_10 = random.randint(10,50)
+    number_11 = random.randint(10,50)
+
+    answer = int(input(f'{number_10} +  {number_11} = '))
+
+    if number_10 + number_11 == answer:
+        print('A válasz helyes')
+    else:
+        print(f'A helyes válasz: {number_11+number_10}')
+
+feladat39()
+
+def feladat40():
+
+    number_12 = random.randint(0,100)
+    number_13 = int(input('Adj meg egy számot(0-100 között): '))
+
+    if number_12 == number_13: print('Nyert')
+    else: print('Nem nyert')
